14d09f2922620ed1fb8403bd64df532a3a1fefa1
joaolages <joaolages@joaolages.lan>
2017-03-20 19:12:42 +0000

    Seq. version using Sets

--- teste.cpp
+++ teste.cpp
@@ -3,8 +3,67 @@
 #include <string>
 #include <sstream> // stringstream
 #include <fstream>  //files
+#include <set> // set
 
 using namespace std; // no need to type 'std::'
+
+int checkLiveCells(string aux_cell, const set<string> &Cube){
+  int liveCells = 0;
+  int x_aux, y_aux, z_aux;
+  int x,y,z;
+
+  x = aux_cell[0]-'0';
+  y = aux_cell[1]-'0';
+  z = aux_cell[2]-'0';
+
+  // (x-1, y, z)
+  x_aux = x-1;
+  aux_cell[0] = '0' + x_aux;
+  if(Cube.count(aux_cell)==1){ // live cell in this position
+    liveCells++;
+  }
+
+  // (x+1, y, z)
+  x_aux = x+1;
+  aux_cell[0] = '0' + x_aux;
+  if(Cube.count(aux_cell)==1){ // live cell in this position
+    liveCells++;
+  }
+
+  aux_cell[0] = '0' + x; //reset
+
+  // (x, y-1, z)
+  y_aux = y-1;
+  aux_cell[1] = '0' + y_aux;
+  if(Cube.count(aux_cell)==1){ // live cell in this position
+    liveCells++;
+  }
+
+  // (x, y+1, z)
+  y_aux = y+1;
+  aux_cell[1] = '0' + y_aux;
+  if(Cube.count(aux_cell)==1){ // live cell in this position
+    liveCells++;
+  }
+
+  aux_cell[1] = '0' + y; // reset
+
+  // (x, y, z-1)
+  z_aux = z-1;
+  aux_cell[2] = '0' + z_aux;
+  if(Cube.count(aux_cell)==1){ // live cell in this position
+    liveCells++;
+  }
+
+  // (x, y, z+1)
+  z_aux = z+1;
+  aux_cell[2] = '0' + z_aux;
+  if(Cube.count(aux_cell)==1){ // live cell in this position
+    liveCells++;
+  }
+
+  return liveCells;
+}
 
 // Split a string depending on the separation (sep) character
 vector<string> split(const string &text, char sep) {
@@ -18,19 +77,6 @@ vector<string> split(const string &text, char sep) {
   return tokens;
 }
 
-// Structure for data in Zs vectors
-struct data_z_axis{
-  int z;
-  bool alive;
-};
-
-// Structure for data in Ys vectors
-struct data_y_axis{
-  int y;
-  vector<data_z_axis> *ptr_y;
-};
-
-
 int main(int argc, char *argv[]){
 
   if( argc != 3 ){ // 2 arguments needed
@@ -42,91 +88,114 @@ int main(int argc, char *argv[]){
   ifstream infile;
   infile.open(argv[1]);
   string line;
-  bool firstline = true;
 
   int number_gen;
-  vector<vector<data_y_axis>*> Cube_x;
-  data_y_axis data_y;
-  data_z_axis data_z;
-  vector<string> tokens_str;
+  stringstream ss;
+  ss << argv[2];
+  ss >> number_gen;
+  set<string> Cube;
+  bool firstline = true;
 
   while (getline(infile, line))
   {
-    if(firstline){ // get firtline of the file
-      stringstream aux_ss;
-      aux_ss << line;
-      aux_ss >> number_gen;
-      Cube_x.reserve(number_gen); // create x axis
-      for(int i=0; i<Cube_x.size(); ++i)  // Initialize with zeros TODO: Acho que os vectors vêm com random data lá dentro em vez de NULLs. Se já vier mesmo com 0s ou assim, remover isto
-        Cube_x[i] = NULL;
+    if(firstline){
       firstline = false;
-    }
-
-    tokens_str = split(line, ' '); // get x,y,z
-    vector<int> tokens;
-    for(int i=0; i<tokens_str.size(); ++i){
-      stringstream aux_ss;
-      int aux_int;
-      aux_ss << tokens_str[i];
-      aux_ss >> aux_int;
-      tokens.push_back(aux_int);
-    }
-
-    if(Cube_x[tokens[0]] == NULL){ // no y axis in this x
-      vector<data_y_axis> Cube_y;
-      Cube_x[tokens[0]] = &Cube_y; // save pointer to y axis in x vector
-
-      vector<data_z_axis> Cube_z;
-      data_y = {tokens[1], &Cube_z};
-      Cube_y.insert(Cube_y.begin(), data_y);
-
-      data_z = {tokens[2], true};
-      Cube_z.insert(Cube_z.begin(), data_z);
-
-    } else { // there is already a pointer to a vector of y axis in this x
-      vector<data_y_axis> Cube_y = *Cube_x[tokens[0]];
-
-      bool aux_inserted = false;
-      int i;
-      for(i=0; i<Cube_y.size(); ++i){
-        if(Cube_y[i].y > tokens[1]) break; // this specific y does not exist
-        if(Cube_y[i].y == tokens[1]){ // y already exists
-          vector<data_z_axis> Cube_z = *Cube_y[i].ptr_y;
-
-          int j;
-          for(j=0; j<Cube_z.size(); ++j){
-            if(Cube_z[j].z > tokens[2]) break;
-          }
-          data_z = {tokens[2], true};
-          Cube_z.insert(Cube_z.begin()+j, data_z);
-
-          aux_inserted = true;
-          break;
-        }
-      }
-      if(!aux_inserted){
-        vector<data_z_axis> Cube_z;
-        data_y = {tokens[1], &Cube_z};
-        Cube_y.insert(Cube_y.begin()+i, data_y);
-
-        data_z = {tokens[2], true};
-        Cube_z.insert(Cube_z.begin(), data_z);
-      }
-    }
-  } 
-
-  for(int i=0; i<Cube_x.size(); ++i){
-    if(Cube_x[i]!=NULL){
-        vector<data_y_axis> Cube_y = *Cube_x[i];
-        for(int j=0; j<Cube_y.size(); ++j){
-          vector<data_z_axis> Cube_z = *Cube_y[j].ptr_y;
-          for(int g=0; g<Cube_z.size(); ++g){
-            cout << i << " " << Cube_y[j].y << " " << Cube_z[g].z << endl;
-          }
-        }
+    }else{
+      vector<string> tokens_str;
+      tokens_str = split(line, ' '); // get x,y,z
+      std::string result;
+      for (auto const& s : tokens_str) result += s;
+      Cube.insert(result);
     }
   }
   infile.close();
+
+  set<string>::iterator iter;
+  set<string> newCube;
+  int aux_live_cells, x, x2, x_aux, y, y2, y_aux, z, z2, z_aux, aux;
+  string aux_cell;
+
+  for(int i=0; i<number_gen; ++i){
+    for(iter=Cube.begin(); iter!=Cube.end();++iter){
+
+        aux_cell = *iter;
+        x = aux_cell[0]-'0';
+        y = aux_cell[1]-'0';
+        z = aux_cell[2]-'0';
+
+        aux_live_cells = checkLiveCells(aux_cell, Cube);
+
+        if(aux_live_cells>=2 and aux_live_cells<=4) // cell lives
+          newCube.insert(aux_cell);
+
+        // check Dead Neighbours
+
+        // (x-1, y, z)
+        x_aux = x-1;
+        aux_cell[0] = '0' + x_aux;
+        if(Cube.count(aux_cell)==0 and newCube.count(aux_cell)==0){ // death cell in this position and not inserted
+          aux_live_cells = checkLiveCells(aux_cell, Cube);
+          if(aux_live_cells>=2 and aux_live_cells<=3) // cell lives
+            newCube.insert(aux_cell);
+        }
+
+        // (x+1, y, z)
+        x_aux = x+1;
+        aux_cell[0] = '0' + x_aux;
+        if(Cube.count(aux_cell)==0 and newCube.count(aux_cell)==0){ // death cell in this position and not inserted
+          aux_live_cells = checkLiveCells(aux_cell, Cube);
+          if(aux_live_cells>=2 and aux_live_cells<=3) // cell lives
+            newCube.insert(aux_cell);
+        }
+
+        aux_cell[0] = '0' + x; //reset
+
+        // (x, y-1, z)
+        y_aux = y-1;
+        aux_cell[1] = '0' + y_aux;
+        if(Cube.count(aux_cell)==0 and newCube.count(aux_cell)==0){ // death cell in this position and not inserted
+          aux_live_cells = checkLiveCells(aux_cell, Cube);
+          if(aux_live_cells>=2 and aux_live_cells<=3) // cell lives
+            newCube.insert(aux_cell);
+        }
+
+        // (x, y+1, z)
+        y_aux = y+1;
+        aux_cell[1] = '0' + y_aux;
+        if(Cube.count(aux_cell)==0 and newCube.count(aux_cell)==0){ // death cell in this position and not inserted
+          aux_live_cells = checkLiveCells(aux_cell, Cube);
+          if(aux_live_cells>=2 and aux_live_cells<=3) // cell lives
+            newCube.insert(aux_cell);
+        }
+
+        aux_cell[1] = '0' + y; // reset
+
+        // (x, y, z-1)
+        z_aux = z-1;
+        aux_cell[2] = '0' + z_aux;
+        if(Cube.count(aux_cell)==0 and newCube.count(aux_cell)==0){ // death cell in this position and not inserted
+          aux_live_cells = checkLiveCells(aux_cell, Cube);
+          if(aux_live_cells>=2 and aux_live_cells<=3) // cell lives
+            newCube.insert(aux_cell);
+        }
+
+        // (x, y, z+1)
+        z_aux = z+1;
+        aux_cell[2] = '0' + z_aux;
+        if(Cube.count(aux_cell)==0 and newCube.count(aux_cell)==0){ // death cell in this position and not inserted
+          aux_live_cells = checkLiveCells(aux_cell, Cube);
+          if(aux_live_cells>=2 and aux_live_cells<=3) // cell lives
+            newCube.insert(aux_cell);
+        }
+    }
+    Cube = newCube;
+    newCube.clear();
+  }
+
+  for(iter=Cube.begin(); iter!=Cube.end();++iter){
+    aux_cell = *iter;
+    cout << aux_cell[0] << " " << aux_cell[1] << " " << aux_cell[2] <<endl;
+  }
 
   return 0;
 }
