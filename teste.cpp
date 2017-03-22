a9b5e7db9d60349ddfb5750b9356e930ec596550
joaolages <joaolages@joaolages.lan>
2017-03-22 22:17:13 +0000

    Sequencial version with Sets

--- teste.cpp
+++ teste.cpp
@@ -8,30 +8,42 @@
 using namespace std; // no need to type 'std::'
 int cube_size;
 
+struct Cell{
+  int coords[3];
+};
 
-int checkLiveCells(string aux_cell, const set<string> &Cube){
+// for ordering set
+inline bool operator<(const Cell& lhs, const Cell& rhs)
+{
+  if(lhs.coords[0] != rhs.coords[0]) return lhs.coords[0] < rhs.coords[0];
+  if(lhs.coords[1] != rhs.coords[1]) return lhs.coords[1] < rhs.coords[1];
+  return lhs.coords[2] < rhs.coords[2];
+}
+
+// Count live cells around a cell
+int checkLiveCells(Cell aux_cell, const set<Cell> &Cube){
   int liveCells = 0;
   int i, aux;
   int cord_vector[3];
 
   for(int i=0;i<3;i++)
-    cord_vector[i] = aux_cell[i]-'0';
+    cord_vector[i] = aux_cell.coords[i];
 
   for(i=0;i<3;i++){
 
     aux = cord_vector[i]-1;
     if(aux==-1) aux=cube_size-1;
-    aux_cell[i] = '0' + aux;
+    aux_cell.coords[i] = aux;
     if(Cube.count(aux_cell)==1) // live cell in this position
       liveCells++;
 
     aux = cord_vector[i]+1;
     if(aux==cube_size) aux=0;
-    aux_cell[i] = '0' + aux;
+    aux_cell.coords[i] = aux;
     if(Cube.count(aux_cell)==1) // live cell in this position
       liveCells++;
 
-    aux_cell[i] = '0' + cord_vector[i];
+    aux_cell.coords[i] = cord_vector[i];
 
   }
 
@@ -51,7 +63,7 @@ vector<string> split(const string &text, char sep) {
 }
 
 
-void readInputFile(string filename, set<string> &Cube){
+void readInputFile(string filename, set<Cell> &Cube){
 
   ifstream infile;
   infile.open(filename);
@@ -68,10 +80,11 @@ void readInputFile(string filename, set<string> &Cube){
       firstline = false;
     }else{
       vector<string> tokens_str;
+      Cell cell;
       tokens_str = split(line, ' '); // get x,y,z
-      std::string result;
-      for (auto const& s : tokens_str) result += s;
-      Cube.insert(result);
+      for(int i=0; i<tokens_str.size(); ++i)
+        cell.coords[i] = atoi(tokens_str[i].c_str());
+      Cube.insert(cell);
     }
   }
   infile.close();
@@ -79,13 +92,13 @@ void readInputFile(string filename, set<string> &Cube){
 
 
 // Program Output
-void printCube(const set<string> &Cube){
-  set<string>::iterator iter;
-  string aux_cell;
+void printCube(const set<Cell> &Cube){
+  set<Cell>::iterator iter;
+  Cell aux_cell;
 
   for(iter=Cube.begin(); iter!=Cube.end();++iter){
     aux_cell = *iter;
-    cout << aux_cell[0] << " " << aux_cell[1] << " " << aux_cell[2] <<endl;
+    cout << aux_cell.coords[0] << " " << aux_cell.coords[1] << " " << aux_cell.coords[2] <<endl;
   }
   return;
 }
@@ -98,7 +111,7 @@ int main(int argc, char *argv[]){
   }
 
   // Read from file
-  set<string> Cube;
+  set<Cell> Cube;
   int number_gen;
   stringstream ss;
   ss << argv[2];
@@ -106,10 +119,10 @@ int main(int argc, char *argv[]){
 
   readInputFile(argv[1], Cube);
 
-  set<string>::iterator iter;
-  set<string> newCube;
+  set<Cell>::iterator iter;
+  set<Cell> newCube;
   int aux_live_cells, aux;
-  string aux_cell;
+  Cell aux_cell;
 
   for(int i=0; i<number_gen; ++i){
     for(iter=Cube.begin(); iter!=Cube.end();++iter){
@@ -119,7 +132,7 @@ int main(int argc, char *argv[]){
         int cord_vector[3];
 
         for(int i=0;i<3;i++)
-          cord_vector[i] = aux_cell[i]-'0';
+          cord_vector[i] = aux_cell.coords[i];
 
         aux_live_cells = checkLiveCells(aux_cell, Cube);
 
@@ -131,7 +144,7 @@ int main(int argc, char *argv[]){
         for(int j = 0;j < 3;j++){
           aux = cord_vector[j]-1;
           if(aux==-1) aux=cube_size-1;
-          aux_cell[j] = '0' + aux;
+          aux_cell.coords[j] = aux;
           if(Cube.count(aux_cell)==0 and newCube.count(aux_cell)==0){ // death cell in this position and not inserted
             aux_live_cells = checkLiveCells(aux_cell, Cube);
             if(aux_live_cells>=2 and aux_live_cells<=3) // cell lives
@@ -140,14 +153,14 @@ int main(int argc, char *argv[]){
 
           aux = cord_vector[j]+1;
           if(aux==cube_size) aux=0;
-          aux_cell[j] = '0' + aux;
+          aux_cell.coords[j] = aux;
           if(Cube.count(aux_cell)==0 and newCube.count(aux_cell)==0){ // death cell in this position and not inserted
             aux_live_cells = checkLiveCells(aux_cell, Cube);
             if(aux_live_cells>=2 and aux_live_cells<=3) // cell lives
               newCube.insert(aux_cell);
           }
 
-          aux_cell[j] = '0' + cord_vector[j];
+          aux_cell.coords[j] = cord_vector[j];
         }
     }
     Cube = newCube;
